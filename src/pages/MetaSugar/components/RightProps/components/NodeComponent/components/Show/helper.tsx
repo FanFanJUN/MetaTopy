c762ab22252e57062278620861133098dfd84c6b
LiCai <1981824361@qq.com>
2023-09-01 10:38:38 +0800

    feat: 参数完善

--- src/pages/MetaSugar/components/RightProps/components/NodeComponent/components/Show/helper.tsx
+++ src/pages/MetaSugar/components/RightProps/components/NodeComponent/components/Show/helper.tsx
@@ -1,3 +1,8 @@
+import { ReactComponent as S1 } from '@/assets/svg/lineStyle/1.svg';
+import { ReactComponent as S2 } from '@/assets/svg/lineStyle/2.svg';
+import { ReactComponent as S3 } from '@/assets/svg/lineStyle/3.svg';
+import { ReactComponent as S4 } from '@/assets/svg/lineStyle/4.svg';
+
 export const Show_List = [
   {
     name: '位置和大小',
@@ -22,6 +27,126 @@ export const Show_List = [
         name: '高',
         toolKey: 'height',
         type: 'inputNumber',
+      },
+      {
+        name: '圆角',
+        toolKey: 'borderRadius',
+        type: 'inputNumber',
+      },
+    ],
+  },
+  {
+    name: '样式',
+    toolKey: 'style',
+    cList: [
+      {
+        name: '线条样式',
+        toolKey: 'dash',
+        defaultValue: 0,
+        type: 'select',
+        options: [
+          { value: 0, label: <S1 /> },
+          { value: 1, label: <S2 /> },
+          { value: 2, label: <S3 /> },
+          { value: 3, label: <S4 /> },
+        ],
+      },
+      {
+        name: '线条颜色',
+        toolKey: 'color',
+        type: 'colorPicker',
+      },
+      {
+        name: '线条宽度',
+        toolKey: 'lineWidth',
+        type: 'inputNumber',
+      },
+      {
+        name: '背景颜色',
+        toolKey: 'background',
+        type: 'colorPicker',
+      },
+      {
+        name: '透明度',
+        toolKey: 'globalAlpha',
+        type: 'inputNumber',
+      },
+      {
+        name: '鼠标经过颜色',
+        toolKey: 'hoverColor',
+        type: 'colorPicker',
+      },
+    ],
+  },
+  {
+    name: '文字',
+    toolKey: 'text',
+    cList: [
+      {
+        name: '文本内容',
+        toolKey: 'text',
+        type: 'textArea',
+      },
+      {
+        name: '字体大小',
+        toolKey: 'fontSize',
+        type: 'inputNumber',
+      },
+      {
+        name: '字体颜色',
+        toolKey: 'textColor',
+        type: 'switch',
+      },
+      {
+        name: '隐藏文字',
+        toolKey: 'hiddenText',
+        type: 'switch',
+      },
+      {
+        name: '水平对齐',
+        toolKey: 'textAlign',
+        type: 'select',
+        options: [
+          { value: 'left', label: '左对齐' },
+          { value: 'center', label: '居中' },
+          { value: 'right', label: '右对齐' },
+        ],
+      },
+      {
+        name: '垂直对齐',
+        toolKey: 'textBaseline',
+        type: 'select',
+        options: [
+          { value: 'top', label: '顶部对齐' },
+          { value: 'middle', label: '居中' },
+          { value: 'bottom', label: '底部对齐' },
+        ],
+      },
+    ],
+  },
+  {
+    name: '禁止',
+    toolKey: 'disable',
+    cList: [
+      {
+        name: '禁止输入',
+        toolKey: 'disableInput',
+        type: 'switch',
+      },
+      {
+        name: '禁用旋转',
+        toolKey: 'disableRotate',
+        type: 'switch',
+      },
+      {
+        name: '禁用缩放',
+        toolKey: 'disableSize',
+        type: 'switch',
+      },
+      {
+        name: '禁用锚点',
+        toolKey: 'disableAnchor',
+        type: 'switch',
       },
     ],
   },
--- src/pages/MetaSugar/components/RightProps/components/NodeComponent/components/Show/index.tsx
+++ src/pages/MetaSugar/components/RightProps/components/NodeComponent/components/Show/index.tsx
@@ -3,9 +3,10 @@ import styles from './index.less';
 import { Show_List } from './helper';
 import { map } from 'lodash';
 import { ArrowDownOutlined, ArrowRightOutlined } from '@ant-design/icons';
-import { Input, InputNumber } from 'antd';
+import { Input, InputNumber, Select, Switch } from 'antd';
 import { useMeta } from '@/pages/MetaSugar/context';
 import { useSetState } from 'ahooks';
+import ColorPicker from '@/pages/component/ColorPicker';
 interface IAppProps {}
 
 const Show: React.FunctionComponent<IAppProps> = (props) => {
@@ -18,19 +19,65 @@ const Show: React.FunctionComponent<IAppProps> = (props) => {
   const { meta2d } = useMeta();
 
   const _handleChange = (value: any, values: any, tKey: string) => {
-    meta2d.setValue({ id: values.id, [tKey]: value });
+    const penOption = { id: values.id, [tKey]: value };
+    if (tKey === 'dash') {
+      const dashOption = {
+        1: [5, 5],
+        2: [10, 10],
+        3: [10, 10, 2, 10],
+      };
+      Object.assign(penOption, {
+        lineDash: dashOption[value] || undefined,
+      });
+    }
+    meta2d.setValue(penOption);
     meta2d.render();
     setState({ fresh: state.fresh + 1 });
   };
 
   const _renderComp = (item: any) => {
     const values = meta2d.store.active?.[0] || {};
-    if ('inputNumber') {
+    if (item.type === 'inputNumber') {
       return (
         <InputNumber
           value={values?.[item.toolKey]}
           style={{ width: '100%' }}
           onChange={(value) => _handleChange(value, values, item.toolKey)}
+        />
+      );
+    } else if (item.type === 'switch') {
+      return (
+        <Switch
+          checked={values?.[item.toolKey]}
+          onChange={(value) => _handleChange(value, values, item.toolKey)}
+        />
+      );
+    } else if (item.type === 'textArea') {
+      return (
+        <Input.TextArea
+          value={values?.[item.toolKey]}
+          onChange={(e) => _handleChange(e.target.value, values, item.toolKey)}
+        />
+      );
+    } else if (item.type === 'select') {
+      return (
+        <Select
+          value={values?.[item.toolKey] ?? item.defaultValue}
+          style={{ width: '100%' }}
+          onChange={(e) => _handleChange(e, values, item.toolKey)}
+        >
+          {item.options.map((o) => {
+            return <Select.Option value={o.value}>{o.label}</Select.Option>;
+          })}
+        </Select>
+      );
+    } else if (item.type === 'colorPicker') {
+      return (
+        <ColorPicker
+          value={values?.[item.toolKey]}
+          onChange={(color?: string | undefined) =>
+            _handleChange(color, values, item.toolKey)
+          }
         />
       );
     }
@@ -40,10 +87,15 @@ const Show: React.FunctionComponent<IAppProps> = (props) => {
   const _renderContent = (obj: any) => {
     switch (obj.toolKey) {
       case 'basic':
+      case 'disable':
+      case 'text':
+      case 'style':
         return obj.cList.map((item) => {
           return (
             <div className={styles.showWrap_item_clist}>
-              <span>{item.name}</span>
+              <span style={{ color: '#7f838c', fontSize: '12px' }}>
+                {item.name}
+              </span>
               <span style={{ width: 150 }}>{_renderComp(item)}</span>
             </div>
           );
@@ -51,6 +103,16 @@ const Show: React.FunctionComponent<IAppProps> = (props) => {
       default:
         break;
     }
+  };
+
+  const _handleEx = (r: any, isShow: boolean) => {
+    let fList: string[] = [];
+    if (isShow) {
+      fList = state.expandKeys.filter((i) => i !== r.toolKey);
+    } else {
+      fList = [...state.expandKeys, r.toolKey];
+    }
+    setState({ expandKeys: fList });
   };
 
   return (
@@ -61,9 +123,14 @@ const Show: React.FunctionComponent<IAppProps> = (props) => {
           <div key={item.toolKey} className={styles.showWrap_item}>
             <div className={styles.showWrap_item_name}>
               <span>{item.name}</span>
-              {isShow ? <ArrowDownOutlined /> : <ArrowRightOutlined />}
+              <span
+                onClick={() => _handleEx(item, isShow)}
+                style={{ cursor: 'pointer' }}
+              >
+                {isShow ? <ArrowDownOutlined /> : <ArrowRightOutlined />}
+              </span>
             </div>
-            {_renderContent(item)}
+            {isShow && _renderContent(item)}
           </div>
         );
       })}
